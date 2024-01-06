3a209f048a7167f3c95b407ece21399c9fc332be
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-06 21:01:36 +0530

    needs some debugging in JS file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const btn = document.getElementById("btn");
 const tip = document.getElementById("tip");
 
 btn.addEventListener("click", () => {
-  if (weight.value !== "" || height.value !== "") {
+  if (weight.value == "" || height.value == "") {
     console.log("Please provide some +ve input");
     tip.innerText = "Please provide some +ve input";
   }
@@ -13,15 +13,16 @@ btn.addEventListener("click", () => {
     console.log("Weight or Height should be +ve Number");
     tip.innerText = "Weight or Height should be +ve Number";
   } else {
-    bmi.value = +weight.value / (height.value / 100) ** 2;
-    if (bmi.value <= 18.5) {
+    calculatedBMI = +weight.value / (height.value / 100) ** 2;
+    bmi.value = calculatedBMI.toFixed(2);
+    if (calculatedBMI <= 18.4) {
       tip.innerText = "You are Underweight, increase your calories.";
-    } else if (18.6 <= bmi.value <= 24.9) {
+    } else if (calculatedBMI >= 18.5 || calculatedBMI <= 24.9) {
       tip.innerText = "You've Normal weight, continue eating healhty";
-    } else if (25 <= bmi.value <= 29.9) {
+    } else if (calculatedBMI >= 25 || calculatedBMI <= 39.9) {
       tip.innerText = "You are Overweight, eat less calories.";
     } else {
-      tip.innerText = "You've Obesity problem contact docker.";
+      tip.innerText = "You've Obesity problem contact physician.";
     }
   }
 });
